cec873fa82d154038385843d8ad352549466f7d3
Lebedev Egor <egorikl@list.ru>
2021-04-11 02:14:25 +0300

    change parse and add canParse to Digit

--- src/main/java/Digit.java
+++ src/main/java/Digit.java
@@ -9,10 +9,13 @@ public class Digit {
         this.digit = digit;
     }
 
+    public static boolean canParse(StringLeftover toParse) {
+        return toParse.left() > 0 && Character.isDigit(toParse.charAt(0));
+    }
+
     public static ParsePair<Digit> parse(StringLeftover toParse) throws ParseException {
-        int digit = Character.getNumericValue(toParse.charAt(0));
-        if (digit < 0 || digit > 9)
+        if (Character.isDigit(toParse.charAt(0)))
             throw new ParseException("Digit is not actually a digit", toParse.offset());
-        return new ParsePair<>(new Digit(digit), toParse.skip(1));
+        return new ParsePair<>(new Digit(Character.getNumericValue(toParse.charAt(0))), toParse.skip(1));
     }
 }
