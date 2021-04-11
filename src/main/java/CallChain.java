a9be94b090346965a8290e3b890b0ba34bcb6e45
Lebedev Egor <egorikl@list.ru>
2021-04-11 12:34:17 +0300

    add CallChain

---
+++ src/main/java/CallChain.java
@@ -0,0 +1,38 @@
+import java.text.ParseException;
+
+public class CallChain {
+    public final Call head;
+    public final CallChain tail;
+
+    public CallChain(Call head) {
+        this.head = head;
+        this.tail = null;
+    }
+
+    public CallChain(Call head, CallChain tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    @Override
+    public String toString() {
+        if (tail == null) return head.toString();
+        else return head + "%>%" + tail;
+    }
+
+    public static boolean canParse(StringLeftover toParse) {
+        return Call.canParse(toParse);
+    }
+
+    public static ParsePair<CallChain> parse(StringLeftover toParse) throws ParseException {
+        if (!canParse(toParse))
+            throw new ParseException("Could not parse call-chain", toParse.offset());
+        var pairH = Call.parse(toParse);
+        if (canParse(pairH.leftover())) {
+            var pairT = CallChain.parse(pairH.leftover());
+            return new ParsePair<>(new CallChain(pairH.parsed(), pairT.parsed()), pairT.leftover());
+        } else {
+            return new ParsePair<>(new CallChain(pairH.parsed()), pairH.leftover());
+        }
+    }
+}
